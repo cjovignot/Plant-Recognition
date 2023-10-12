b3ac69041c84d6d10e03c3645edc546d6bb4c40b
cjovignot <cosme.jovignot@epitech.eu>
2023-10-12 19:23:24 +0200

    [VERCEL]

--- app/level1/page.jsx
+++ app/level1/page.jsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useSearchParams } from 'next/navigation'
-import Link from "next/link";
 
 
 const getPlants = async (params) => {

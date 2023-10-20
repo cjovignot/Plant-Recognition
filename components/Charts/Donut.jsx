768894e17abd552b5ec4efc9291c269c18ec380a
cjovignot <cosme.jovignot@epitech.eu>
2023-10-20 17:05:24 +0200

    [ADD] chart in scores

--- components/Charts/Donut.jsx
+++ components/Charts/Donut.jsx
@@ -4,22 +4,59 @@ import Chart from 'react-apexcharts';
 function Donut({ series }) {
     console.log("series =>", series)
 
+    const correct = series[0];
+    const incorrect = series[1];
+    const ratio = `${correct}/${incorrect}`;
+
     const options = {
-        labels: ['Correct', 'False'],
+        labels: ['Validé', 'A revoir'],
         chart: {
             type: 'donut',
         },
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                chart: {
-                    width: 200
-                },
-                legend: {
-                    position: 'bottom'
+        colors: ['#49a14c', '#f35b5b'],
+        legend: {
+            position: 'bottom'
+        },
+        plotOptions: {
+            pie: {
+                customScale: 0.8,
+                donut: {
+                    labels: {
+                        show: true,
+                        name: {
+                            show: true,
+                            fontSize: '22px',
+                            fontFamily: 'Helvetica, Arial, sans-serif',
+                            fontWeight: 600,
+                            color: '#373d3f',
+                            offsetY: -10
+                        },
+                        value: {
+                            show: true,
+                            fontSize: '16px',
+                            fontFamily: 'Helvetica, Arial, sans-serif',
+                            fontWeight: 400,
+                            color: '#373d3f',
+                            offsetY: 16,
+                            formatter: function (val) {
+                                return val;
+                            }
+                        },
+                        total: {
+                            show: true,
+                            showAlways: true,
+                            label: 'Total',
+                            fontSize: '22px',
+                            fontWeight: 600,
+                            color: '#373d3f',
+                            formatter: function (w) {
+                                return ratio;
+                            }
+                        }
+                    }
                 }
             }
-        }],
+        },
     };
 
     const [chartData, setChartData] = useState({
--- components/Scores.jsx
+++ components/Scores.jsx
@@ -3,25 +3,25 @@ import React, { useEffect, useState } from 'react';
 import Donut from '@/components/Charts/Donut'
 
 export default function Scores({ trueArray, falseArray }) {
+    const series = [trueArray.length, falseArray.length]
+    
     console.log("correct", trueArray)
     console.log("false", falseArray)
-    
 
-    const series = [trueArray.length, falseArray.length];
-    
+
     return (
         <>      
             <div className="h-screen flex justify-center items-center m-auto lg:pt-20">
-            <div className="bg-base-100 shadow-xl lg:rounded-3xl">
-                <div className="flex flex-col h-screen lg:h-[600px] w-screen lg:max-w-[1000px] justify-between">
-                    <div className="flex flex-col justify-around items-center pb-10">
-                    <h2 className="pt-20 card-title text-2xl">Résultats</h2>
-                    <p>{trueArray.length}/{falseArray.length}</p>
-
-                    <Donut series={series}/>
+                <div className="bg-base-100 shadow-xl lg:rounded-3xl">
+                    <div className="flex flex-col h-screen lg:h-[600px] w-screen lg:max-w-[1000px] justify-between">
+                        <div className="flex flex-col items-center">
+                            <h2 className="pt-20 card-title text-2xl">Résultats</h2>
+                            <div>
+                                <Donut series={series}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </>
     );

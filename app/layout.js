23d1ffcd29f791270545353cc8dfe6570b4dbba9
cjovignot <cosme.jovignot@epitech.eu>
2023-10-26 18:05:37 +0200

    [ADD] Search in settings

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
       </Head>
+      <head><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /></head>
       <body className={inter.className}>
         <div className="">
           <Navbar />

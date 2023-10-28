91264c942b54611b8831962989f14960eed1c926
cjovignot <cosme.jovignot@epitech.eu>
2023-10-28 23:42:09 +0200

    [ADD] vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ToastContainer } from 'react-toastify';
+import { Analytics } from '@vercel/analytics/react';
 import 'react-toastify/dist/ReactToastify.css';
 import Head from "next/head";
 import Link from "next/link";
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
         <div className="">
           <Navbar />
           <div>{children}</div>
+          <Analytics />
           <ToastContainer />
         </div>
       </body>
